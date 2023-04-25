dd24156119ac841e419564ec53b9d341e506bc60
Mihail <mohonovproduction@yandex.ru>
2023-04-25 20:46:50 +0300

    .env

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ require('dotenv').config()
 
 const bot = new Telegraf(process.env.BOT_TOKEN)
 
-bot.start(ctx => ctx.reply('Hello, World!'))
+bot.start(ctx => {
+    console.log(ctx.message)
+    ctx.reply('Hello, World!')
+})
 
 bot.launch()
