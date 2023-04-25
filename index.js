8013d176bd9d239d052146ea3dd572a25af095c8
Mihail <mohonovproduction@yandex.ru>
2023-04-25 20:39:38 +0300

    start command

---
+++ index.js
@@ -0,0 +1,8 @@
+const { Telegraf } = require('telegraf')
+require('dotenv').config()
+
+const bot = new Telegraf(process.env.BOT_TOKEN)
+
+bot.start(ctx => ctx.reply('Hello, World!'))
+
+bot.launch()
