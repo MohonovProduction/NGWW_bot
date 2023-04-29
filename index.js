78ca21ebd90e7da90dae4e4b09a46194ad848139
Mihail <mohonovproduction@yandex.ru>
2023-04-30 01:07:55 +0300

    send request

--- index.js
+++ index.js
@@ -1,11 +1,44 @@
-const { Telegraf } = require('telegraf')
-require('dotenv').config()
+const { Telegraf, session, Scenes } = require('telegraf')
+const Config = require('./config.json')
 
-const bot = new Telegraf(process.env.BOT_TOKEN)
+require('dotenv').config({ path: "./.env" })
 
-bot.start(ctx => {
+console.log(process.env)
+
+const Stage = new Scenes.Stage()
+
+const request = new Scenes.WizardScene(
+    'request',
+    ctx => {
+        console.log(ctx)
+        ctx.reply('Как к вам обращаться?')
+        return ctx.wizard.next()
+    },
+    ctx => {
+        ctx.wizard.state['name'] = ctx.message.text
+        ctx.reply('Как с вами связаться?')
+        return ctx.wizard.next()
+    },
+    ctx => {
+        ctx.wizard.state['number'] = ctx.message.text
+        ctx.telegram.sendMessage(process.env.ADMIN_CHAT, `Новая заявка:\n${ctx.wizard.state.name} ${ctx.wizard.state.number}`)
+        ctx.reply('Спасибо за Ваше обращение, наш специалист скоро свяжется с Вами')
+    }
+)
+
+Stage.register(request)
+
+const bot = new Telegraf(process.env.TOKEN)
+
+bot
+    .use(session())
+    .use(Stage.middleware())
+
+bot.telegram.setMyCommands(Config.commands)
+
+bot.start( ctx => {
     console.log(ctx.message)
-    ctx.reply('Hello, World!')
+    ctx.scene.enter('request')
 })
 
 bot.launch()
