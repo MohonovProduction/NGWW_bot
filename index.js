30f84a78c366780662c443f73d22e552fb312267
Mihail <mohonovproduction@yandex.ru>
2023-04-30 14:02:42 +0300

    undefind check, remove keyboard after using

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { Telegraf, session, Scenes } = require('telegraf')
+const { Telegraf, session, Scenes, Markup } = require('telegraf')
 const Config = require('./config.json')
 
 require('dotenv').config({ path: "./.env" })
@@ -9,20 +9,61 @@ const Stage = new Scenes.Stage()
 
 const request = new Scenes.WizardScene(
     'request',
-    ctx => {
-        console.log(ctx)
-        ctx.reply('Как к вам обращаться?')
+    async ctx => {
+        await ctx.reply(
+            'Как к вам обращаться?',
+            {
+                reply_markup: {
+                    remove_keyboard: true
+                }
+            }
+        )
         return ctx.wizard.next()
     },
-    ctx => {
+    async ctx => {
         ctx.wizard.state['name'] = ctx.message.text
-        ctx.reply('Как с вами связаться?')
+        await ctx.reply(
+            'Как с вами связаться?',
+            {
+                reply_markup: {
+                    keyboard: [
+                        [new Markup.button.contactRequest('Отправить контакты')]
+                    ],
+                    one_time_keyboard: true,
+                    resize_keyboard: true
+                }
+            }
+        )
+
         return ctx.wizard.next()
     },
-    ctx => {
-        ctx.wizard.state['number'] = ctx.message.text
-        ctx.telegram.sendMessage(process.env.ADMIN_CHAT, `Новая заявка:\n${ctx.wizard.state.name} ${ctx.wizard.state.number}`)
-        ctx.reply('Спасибо за Ваше обращение, наш специалист скоро свяжется с Вами')
+    async ctx => {
+        if (!('contact' in ctx.message)) {
+            await ctx.reply('Пожалуйста, отправтье Ваши контакты, чтобы мы могли связаться с вами')
+            return ctx.wizard.back()
+        }
+
+        ctx.wizard.state['contact'] = ctx.message.contact
+
+        await ctx.telegram.sendMessage(
+            process.env.ADMIN_CHAT,
+            `<b>Новая заявка:</b>\n\n${ctx.wizard.state.name} / tg: ${ctx.wizard.state.contact.first_name}\n\n<pre>${ctx.wizard.state.contact.phone_number}</pre>`,
+            {
+                parse_mode: 'HTML',
+                reply_markup: {
+                    remove_keyboard: true
+                }
+            }
+        )
+        await ctx.telegram.sendContact(
+            process.env.ADMIN_CHAT,
+            ctx.wizard.state.contact.phone_number,
+            ctx.wizard.state.contact.first_name,
+        )
+
+        await ctx.reply('Спасибо за Ваше обращение, наш специалист скоро свяжется с Вами')
+
+        return ctx.scene.leave()
     }
 )
 
